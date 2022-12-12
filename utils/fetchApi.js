820f70259116251d65579cda6246456cc2dd807b
André Abreu de Sá <94800180+MaxSyos@users.noreply.github.com>
2022-12-12 16:23:54 -0300

    Update

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY ,
+      'x-rapidapi-key': '152fdc5502mshe268a06f162f6e0p1a0b54jsn1400e05af997' ,
     },
   });
     
